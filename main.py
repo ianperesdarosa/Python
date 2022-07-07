90a05b1595df2b0cc60f24f7ca7d7e5385a7711a
ianperesdarosa <ianperesdarosa2004@gmail.com>
2022-07-07 11:11:44 -0300

    :fire:

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Insira suas informações")
+
+nome = input("Seu nome:")
+idade = input("Sua idade:")
+sexo = input("Seu sexo:")
+
+print("Sua documentaçâo está pronta!")
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Sexo:", sexo)
