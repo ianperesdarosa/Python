3bda9b82ebe6e92ac4dc48c8b0792b96d87441c6
ianperesdarosa <ianperesdarosa2004@gmail.com>
2023-04-16 21:57:06 -0300

    :fire:

---
+++ Playground-code/01-main.py
@@ -0,0 +1,7 @@
+print("Vamos descobrirmos se o número e Par ou Ímpar?")
+value = input("Digite o número:")
+
+if (value % 2) == 0:
+    print("{value} é um número Par..") 
+else:
+    print("{value} é um número Ímpar..") 
