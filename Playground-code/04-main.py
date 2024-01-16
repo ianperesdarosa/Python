1d02ca974d3455a9602f733a262f1df4b47ed672
ianperesdarosa <91580202+ianperesdarosa@users.noreply.github.com>
2024-01-15 22:04:50 -0300

    commit

--- Playground-code/04-main.py
+++ Playground-code/04-main.py
@@ -13,17 +13,22 @@ operation = int(input("Escolha da operação: "))
 
 if operation == 1:
     res = (n1 + n2)
-    print(res)   
+    print(res)  
+
 elif operation == 2:
     res = (n1 - n2)
     print(res) 
+
 elif operation == 3:
     res = (n1 * n2)
     print(res) 
+
 elif operation == 4:
     res = (n1 / n2)
     print(res) 
+
 elif operation == 5:
     print("Exit!")
+    
 else:
     print("Error!")
