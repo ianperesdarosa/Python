f8db219f07be3839b8f3198b42b9a805919acb41
ianperesdarosa <ianperesdarosa2004@gmail.com>
2024-12-06 22:09:54 -0300

    commit

--- Playground-code/07-main.py
+++ Playground-code/07-main.py
@@ -9,11 +9,9 @@ class StatusInvestImporter:
         self.dados = None
 
     def importar_dados(self):
-        # Importar dados do Status Invest
         self.dados = pd.read_csv(self.link)
 
     def gerar_graficos(self):
-        # Gerar gráficos das ações
         for acao in self.dados['Ação'].unique():
             dados_acao = self.dados[self.dados['Ação'] == acao]
             plt.figure(figsize=(10, 6))
@@ -24,12 +22,10 @@ class StatusInvestImporter:
             plt.savefig(f'{acao}.png')
 
     def exportar_para_power_bi(self):
-        # Exportar gráficos para o Power BI
         power_bi = PowerBI()
         for acao in self.dados['Ação'].unique():
             power_bi.import_image(f'{acao}.png', f'Gráfico de {acao}')
 
-# Exemplo de uso:
 link = 'https://statusinvest.com.br/acoes/acoes.csv'
 importer = StatusInvestImporter(link)
 importer.importar_dados()
